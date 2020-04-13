398e2f38cc993e9c791cc6e4cff5c345c50f4d9a
Javante Nelson <javantenelson@gmail.com>
2020-04-13 12:53:56 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+
+class GetRequester
+     
+    attr_accessor :url 
+     
+  def initialize(url)
+   @url = url 
+  end 
+  
+  def parse_json 
+     
+  end 
+  
+  def get_response_body
+    #binding.pry
+    uri = URI.parse(@url)
+    response = Net::HTTP.get(uri)
+    response.body
+   
+  end 
+  
+   
+end 
